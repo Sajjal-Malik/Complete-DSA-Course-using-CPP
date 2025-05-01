ec54881813a15066d2a4dc558a5b692c2bbf0b67
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-05-01 06:15:32 +0500

    Added new file in Folder 13

--- Folder 13 - Advance Sorting Algorithms/Quick_Sort.cpp
+++ Folder 13 - Advance Sorting Algorithms/Quick_Sort.cpp
@@ -1,6 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int partition(int arr[], int s, int e){
+
+    int pivot = arr[s];
+
+    int cnt = 0;
+    for(int i = s + 1; i <= e; i++){
+        if(arr[i] <= pivot){
+            cnt++;
+        }
+    }
+
+    // place pivot at right position
+    int pivotIndex = s + cnt;
+    swap(arr[pivotIndex], arr[s]);
+
+    // handle left and right parts
+    int i = s, j = e;
+    while(i < pivotIndex && j > pivotIndex){
+        while(arr[i] < pivot){
+            i++;
+        }
+        while(arr[j] > pivot){
+            j--;
+        }
+
+        if(i < pivotIndex && j > pivotIndex){
+            swap(arr[i++], arr[j--]);
+        }
+    }
+
+    return pivotIndex;
+}   
+
 void quickSort(int arr[], int s, int e){
 
     // base case
