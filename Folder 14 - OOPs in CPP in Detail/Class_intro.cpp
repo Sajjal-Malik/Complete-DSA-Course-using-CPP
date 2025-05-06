c87095add4eeeaf2f25b7f2b45f96058f0fbd1d2
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-05-06 07:16:50 +0500

    Created new dir Folder 14

---
+++ Folder 14 - OOPs in CPP in Detail/Class_intro.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Hero{
+    // properties
+    int health;
+};
+
+int main(){
+
+    Hero h1;
+
+    cout << size of << sizeof(h1) << endl;
+
+    return 0;
+}
