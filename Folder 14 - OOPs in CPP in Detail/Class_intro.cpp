78180220f9d07e7b2bd542fdc10b88255c92d529
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-05-13 19:50:13 +0500

    New code Added in Folder 14

--- Folder 14 - OOPs in CPP in Detail/Class_intro.cpp
+++ Folder 14 - OOPs in CPP in Detail/Class_intro.cpp
@@ -1,16 +1,84 @@
 #include<bits/stdc++.h>
+// #include "Hero.cpp"
 using namespace std;
 
-class Hero{
-    // properties
+class Hero{  // empty class takes 1 Byte of Memory
+
+    
+    // properties / Data members
+    private:
     int health;
+    
+    public:
+
+    char level;
+    Hero(){
+        cout << "constructor called" << endl;
+    }
+        
+    void print(){
+        cout << level << endl;
+    }
+
+    // getters ------->>>>>>>
+    int getHealth(){
+        return health;
+    }
+
+    char getLevel(){
+        return level;
+    }
+
+    // setters ------->>>>>>>
+    void setHealth(int hlth){
+        health = hlth;
+    }
+
+    void setLevel(char lvl){
+        level = lvl;
+    }
+
 };
 
 int main(){
 
-    Hero h1;
+    // Hero malik;
+    // malik.setHealth(99);
+    // malik.setLevel('D');
+    
+    // cout << malik.getHealth() << endl;
+    // cout << malik.getLevel() << endl;
 
-    cout << "size of" << sizeof(h1) << endl;
+
+    // static allocation
+    // Hero h1;
+    // h1.level = 'A';
+    // h1.setHealth(89);
+
+    // cout << "Level is: " << h1.level << endl;
+    // cout << "Health is: " << h1.getHealth() << endl;
+    
+
+    // // Dynamic Allocation
+    // Hero *h2 = new Hero();
+
+    // h2->level = 'C';
+    // h2->setHealth(90);
+
+    // cout << "Level is: " << (*h2).level << endl;
+    // cout << "Health is: " << (*h2).getHealth() << endl;
+    
+    // cout << "Level is: " << h2->level << endl;
+    // cout << "Health is: " << h2->getHealth() << endl;
+
+
+    // when an instance is created a constructor is envoked by befault  (Default Constructor)
+
+    // object created statically (constructor called)
+    Hero david;
+
+    // object created dynamically (constructor called)
+    Hero *john = new Hero();
 
     return 0;
 }
---
+++ Folder 14 - OOPs in CPP in Detail/Hero.cpp
@@ -0,0 +1,9 @@
+class Hero{
+
+    // properties / Data members
+
+    char name[100];
+    int health;
+    char level;
+    
+};
