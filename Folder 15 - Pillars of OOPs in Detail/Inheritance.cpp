1b823c34ab09cc8eec43f75e0e0728a4d8f38f7b
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-05-31 17:10:04 +0500

    Modified Folder 15

--- Folder 15 - Pillars of OOPs in Detail/Inheritance.cpp
+++ Folder 15 - Pillars of OOPs in Detail/Inheritance.cpp
@@ -1,23 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Student {
-    private:
-        string name;
-        int age;
+class Human {
+
+    protected:
         int height;
+    public:
+        int weight;
+    
+    private:
+        int age;
 
     public:
         int getAge(){
             return this->age;
         }
+        
+        void setWeight(int weight){
+            this->weight = weight;
+        }
+};
+
+class Male: public Human {
+
+    public:
+        string color;
+
+        void sleep(){
+            cout << "Male is sleeping" << endl;
+        }
+
+        int getHeight(){
+            return this->height;
+        }
 };
 
 int main(){
 
-    Student s1;
+    Male m1;
 
-    cout << s1.getAge() << endl;
+    // cout << m1.age << endl;  // not accessible parent -> private .property or .method()
+
+    // cout << m1.height << endl;  // not accessible outside child -> protected .property or .method()
+    // cout << m1.weight << endl;  // not accessible outside child -> protected .property or .method()
+
+    cout << m1.getHeight() << endl;
+    cout << m1.color << endl;
+    m1.sleep();
 
     return 0;
 }
