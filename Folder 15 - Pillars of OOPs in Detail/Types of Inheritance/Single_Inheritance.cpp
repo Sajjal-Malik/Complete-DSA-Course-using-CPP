d85e722d8d5b2f14054d716e709f349840db8d93
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-06-01 14:56:46 +0500

    Modified Folder 15

--- Folder 15 - Pillars of OOPs in Detail/Types of Inheritance/Single_Inheritance.cpp
+++ Folder 15 - Pillars of OOPs in Detail/Types of Inheritance/Single_Inheritance.cpp
@@ -1,9 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-
+class Animal{
+    public:
+        int age;
+        int weight;
     
+    public:
+        void speak(){
+            cout << "Speaking..." << endl;
+        }
+};
+
+class Dog: public Animal{
+
+
+
+};
+
+int main(){    
+
+    Dog d1;
+    d1.speak();
+
 
     return 0;
 }
